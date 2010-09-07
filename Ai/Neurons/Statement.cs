182474f3d6cad285308684b2f23bd30184424911
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-09-07 23:51:04 +0000

    Added statement class

---
+++ Ai/Neurons/Statement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtificialArt.Ai
+{
+    /// <summary>
+    /// Represents a statement
+    /// </summary>
+    public class Statement
+    {
+        #region Parts
+        /// <summary>
+        /// Subject neuron
+        /// </summary>
+        private Concept subject;
+
+        /// <summary>
+        /// Verb neuron
+        /// </summary>
+        private Concept verb;
+
+        /// <summary>
+        /// Complement neuron
+        /// </summary>
+        private Concept complement;
+
+        /// <summary>
+        /// Whether the statement is true or not
+        /// </summary>
+        private bool isPositive;
+        #endregion
+    }
+}
