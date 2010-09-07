88383f4110a06885690aed761a412c6023708a02
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-09-07 23:25:32 +0000

    Added first files

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ack5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UnitTesting.UnitTestingConceptNameManager.Test();
+        }
+    }
+}
---
+++ UnitTesting/UnitTestingConceptNameManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArtificialArt.Ai;
+
+namespace Ack5.UnitTesting
+{
+    internal static class UnitTestingConceptNameManager
+    {
+        internal static void Test()
+        {
+            ConceptNameManager conceptNameManager = new ConceptNameManager();
+            UnitTesting.AssertEquals(conceptNameManager["cat"] == conceptNameManager["cat"], true);
+            UnitTesting.AssertEquals(conceptNameManager["cat"] == conceptNameManager["dog"], false);
+            UnitTesting.AssertEquals(conceptNameManager["cat"] == conceptNameManager["CaT"], true);
+        }
+    }
+}
