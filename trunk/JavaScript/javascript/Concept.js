eb8fb05e5742f71f53ea14553e7db08d820908bb
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-09-18 18:58:00 +0000

    Can now learn totologies

--- trunk/JavaScript/javascript/Concept.js
+++ trunk/JavaScript/javascript/Concept.js
@@ -4,6 +4,6 @@ function Concept(defaultConceptName)
 	//Default concept name (for debugging and etc)
 	this.defaultConceptName = defaultConceptName;
 	
-	//Concepts connections as subject.connections[verb][complementIndex]complement
+	//Positive concepts connections as subject.connections[verb][complementIndex]complement
 	this.connections = Array();
 }
