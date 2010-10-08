e92808dfc5cbcc5b316739636f915b693e7019e1
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-10-08 21:08:04 +0000

    added Evaluator.render() stub

--- JavaScript/javascript/EvaluationCache.js
+++ JavaScript/javascript/EvaluationCache.js
@@ -1,4 +1,37 @@
 //Stores result of evaluation of propositions to improve performances of evaluation
 function EvaluationCache()
 {
+	//(Array)[subject][verb][complement]
+	//of constant as: Evaluator.resultTrue, Evaluator.resultFalse,
+	//Evaluator.resultUnknown, Evaluator.resultNotInCache
+	this.cachedData = Array();
+}
+
+EvaluationCache.prototype.getCachedResult = function EvaluationCache_getCachedResult(subject, verb, complement, defaultNullValue)
+{
+	if (this.cachedData[subject] == null)
+		this.cachedData[subject] = Array();
+
+	if (this.cachedData[subject][verb] == null)
+		this.cachedData[subject][verb] = Array();
+		
+	if (this.cachedData[subject][verb][complement] == null)
+	{
+		return defaultNullValue;
+	}
+	else
+	{
+		return this.cachedData[subject][verb][complement];
+	}
+}
+
+EvaluationCache.prototype.setCachedResult = function EvaluationCache_setCachedResult(subject, verb, complement, resultToSet)
+{
+	if (this.cachedData[subject] == null)
+		this.cachedData[subject] = Array();
+
+	if (this.cachedData[subject][verb] == null)
+		this.cachedData[subject][verb] = Array();
+		
+	this.cachedData[subject][verb][complement] == result;
 }
