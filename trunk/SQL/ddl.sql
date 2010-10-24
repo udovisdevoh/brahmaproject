7c871683ad87d04aaec6269a7312f616b97f17fd
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-10-24 18:50:06 +0000

    Deleted CakePHP

--- trunk/SQL/ddl.sql
+++ trunk/SQL/ddl.sql
@@ -15,17 +15,17 @@ CREATE TABLE IF NOT EXISTS `ai_units` (
 CREATE TABLE IF NOT EXISTS `ai_unit_states` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `data` longblob NOT NULL,
-  `date` datetime NOT NULL,
+  `modified` datetime NOT NULL,
   `ai_unit_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `ai_unit_id` (`ai_unit_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `user_profiles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `key_name` varchar(248) NOT NULL,
-  `user_name` varchar(248) NOT NULL,
+  `name` varchar(248) NOT NULL,
   `password` varchar(40) NOT NULL,
   `email` varchar(255) NOT NULL,
   `first_name` varchar(255) NOT NULL,
