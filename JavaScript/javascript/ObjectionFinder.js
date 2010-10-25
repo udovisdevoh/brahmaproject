7af66da3728a0fc5f259d663c91ddfa615a8e17c
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-10-25 18:26:43 +0000

    added ObjectionFinder stub

---
+++ JavaScript/javascript/ObjectionFinder.js
@@ -0,0 +1,10 @@
+//To find objections to statements
+function ObjectionFinder()
+{
+}
+
+//(Statement) objection statement or null
+ObjectionFinder.prototype.findObjection = function ObjectionFinder_findObjection(subject, verb, complement)
+{
+	throw 'Implement ObjectionFinder.findObjection()';
+}
