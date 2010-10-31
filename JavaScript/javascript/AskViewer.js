809706faeeb674d210cb491f1f316b6ce95fe8c4
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-10-31 19:36:49 +0000

    Added AskViewer stub

---
+++ JavaScript/javascript/AskViewer.js
@@ -0,0 +1,7 @@
+//Makes AI ask questions in order to learn
+function AskViewer(flattenizer, instinct, conceptNameMapper)
+{
+	this.instinct = instinct;
+	this.flattenizer = flattenizer;
+	this.conceptNameMapper = conceptNameMapper;
+}
