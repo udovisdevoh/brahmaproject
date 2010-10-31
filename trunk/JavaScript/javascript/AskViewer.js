a480ceb7fe83744142fb72566e9da0675c4fa63c
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-10-31 21:22:28 +0000

    AskViewer: work in progress

--- trunk/JavaScript/javascript/AskViewer.js
+++ trunk/JavaScript/javascript/AskViewer.js
@@ -1,7 +1,29 @@
 //Makes AI ask questions in order to learn
 function AskViewer(flattenizer, instinct, conceptNameMapper)
 {
+	//Constants
+	this.maxIgnoreListLength = 10;
+	
+	//Parts
 	this.instinct = instinct;
 	this.flattenizer = flattenizer;
 	this.conceptNameMapper = conceptNameMapper;
+	this.ignoreList = Array();
+}
+
+//(String (HTML))
+AskViewer.prototype.askAbout = function AskViewer_askAbout(subject)
+{
+	var leastDocumentedVerb = this.getLeastDocumentedVerbNotInIgnoreList(subject);
+	while (this.ignoreList.length > this.maxIgnoreListLength)
+		this.ignoreList.splice(0,1);
+	var question = '<span class="AiConcept">' + subject + '</span> <span class="AiOperator">' + verb + '</span> what?';
+	this.ignoreList.push(question);
+	return question;
+}
+
+//(Concept)
+AskViewer.prototype.getLeastDocumentedVerbNotInIgnoreList = function AskViewer_getLeastDocumentedVerbNotInIgnoreList(subject)
+{
+	
 }
