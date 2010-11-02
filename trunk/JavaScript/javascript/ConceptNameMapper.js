f55f47267e140dcb788f2ae883895dbc3da35373
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-02 00:52:38 +0000

    When aliasing, don't allow if find obstruction

--- trunk/JavaScript/javascript/ConceptNameMapper.js
+++ trunk/JavaScript/javascript/ConceptNameMapper.js
@@ -37,8 +37,8 @@ ConceptNameMapper.prototype.getConcept = function ConceptNameMapper_getConcept(c
 }
 
 //Merge two concept names so they point to the same concept
-//The flattenizer is facultative, but highly recommended so we keep consistency
-ConceptNameMapper.prototype.alias = function ConceptNameMapper_alias(conceptName1, conceptName2, flattenizer)
+//The flattenizer and objection finder are facultative, but highly recommended so we keep consistency
+ConceptNameMapper.prototype.alias = function ConceptNameMapper_alias(conceptName1, conceptName2, flattenizer, objectionFinder)
 {
 	conceptName1 = conceptName1.toLowerCase();
 	conceptName2 = conceptName2.toLowerCase();
@@ -50,7 +50,48 @@ ConceptNameMapper.prototype.alias = function ConceptNameMapper_alias(conceptName
 	{
 		return;
 	}
+	
+	
+	
+	
+	//First we try to find an objection to the merge of the two concepts
+	if (objectionFinder != null)
+	{
+		for (var index2 = 0; index2 < concept2.tautologyConnections.keys.length; index2++)
+		{
+			var verb = concept2.tautologyConnections.keys[index2];
+			if (verb instanceof Concept)
+			{
+				var tautologicBranch = concept2.getTautologicBranch(verb);
+				
+				if (flattenizer != null)
+				{
+					var implicitBranch = concept2.getImplicitBranch(verb);
+					if (!implicitBranch.isFlat)
+						if (!implicitBranch.isLocked)
+							flattenizer.flattenBranch(implicitBranch, concept2, verb);
+				}
+				
+				for (var index = 0; index < tautologicBranch.complementList.length; index++)
+				{
+					var complement = tautologicBranch.complementList[index];
+					if (complement instanceof Concept)
+					{
+						var objection = objectionFinder.findObjection(concept1, verb, complement);
+						if (objection != null)
+						{
+							throw 'Cannot do that because <span class="AiConcept">' + objection.subject + '</span> <span class="AiOperator">' + objection.verb + '</span> <span class="AiConcept">' + objection.complement + '</span>';
+						}
+					}
+				}
+			}
+		}
+	}
 
+	
+	
+	
+	//Then we copy the connections
 	for (var index2 = 0; index2 < concept2.tautologyConnections.keys.length; index2++)
 	{
 		var verb = concept2.tautologyConnections.keys[index2];
@@ -72,6 +113,7 @@ ConceptNameMapper.prototype.alias = function ConceptNameMapper_alias(conceptName
 				if (complement instanceof Concept)
 				{
 					this.tautologyManager.addConnection(concept1, verb, complement);
+					this.tautologyManager.removeConnection(concept2, verb, complement);
 										
 					for (var complementaryVerbIndex = 0; complementaryVerbIndex < verb.complementaryOperators.length; complementaryVerbIndex++)
 					{
@@ -95,8 +137,8 @@ ConceptNameMapper.prototype.alias = function ConceptNameMapper_alias(conceptName
 }
 
 //Split two concept names so they don't point to the same concept anymore
-//The flattenizer is facultative, but highly recommended so we keep consistency
-ConceptNameMapper.prototype.unAlias = function ConceptNameMapper_unAlias(conceptName1, conceptName2, flattenizer)
+//The flattenizer and objection finder are facultative, but highly recommended so we keep consistency
+ConceptNameMapper.prototype.unAlias = function ConceptNameMapper_unAlias(conceptName1, conceptName2, flattenizer, objectionFinder)
 {
 	conceptName1 = conceptName1.toLowerCase();
 	conceptName2 = conceptName2.toLowerCase();
@@ -153,18 +195,20 @@ ConceptNameMapper.prototype.unAlias = function ConceptNameMapper_unAlias(concept
 }
 
 //Rename a concept to another one
-//The flattenizer is facultative, but highly recommended so we keep consistency
-ConceptNameMapper.prototype.rename = function ConceptNameMapper_rename(conceptName1, conceptName2, flattenizer)
+//The flattenizer and objection finder are facultative, but highly recommended so we keep consistency
+ConceptNameMapper.prototype.rename = function ConceptNameMapper_rename(conceptName1, conceptName2, flattenizer, objectionFinder)
 {
 	var concept1 = this.getConcept(conceptName1);
 
-	this.alias(conceptName1, conceptName2, flattenizer);
+	this.alias(conceptName1, conceptName2, flattenizer, objectionFinder);
 	
 	conceptName2 = conceptName2.toLowerCase();
 	var concept2 = this.getConcept(conceptName2);
 	
 	this.mapNameToConcept.removeItem(conceptName1);
 	var nameList = this.mapConceptToName.getItem(concept2);
+	
+	concept2.defaultConceptName = conceptName2;
 	
 	for (var index = 0; index < nameList.length; index++)
 		if (nameList[index] == conceptName1)
