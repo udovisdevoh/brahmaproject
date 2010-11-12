66167b9c23e60fc1530682e0ce5543d2293758d5
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-12 22:53:47 +0000

    AutoComplete: work in progress

--- JavaScript/javascript/AutoComplete.js
+++ JavaScript/javascript/AutoComplete.js
@@ -3,6 +3,7 @@ function AutoComplete(conceptNameMapper)
 {
 	//Parts
 	this.conceptNameMapper = conceptNameMapper;
+	this.currentPosition = 0;
 }
 
 //(String) get stub of last word
@@ -12,29 +13,68 @@ AutoComplete.prototype.getLastWordStub = function AutoComplete_getLastWordStub(s
 	statementString = ' ' + statementString;
 
 	var lastSpacePosition = statementString.lastIndexOf(' ');
-	if (lastSpacePosition != -1)
-	{
-		return statementString.substr(lastSpacePosition).trim();
-	}
-	else
-	{
-		return null;
-	}
+	
+	var lastWord = statementString.substr(lastSpacePosition).trim();
+	if (lastWord.length > 0)
+		return lastWord;
+	return null;
 }
 
 //(String[]) list of words starting with wordStub
+//null if nothing matched
 AutoComplete.prototype.getWordList = function AutoComplete_getWordList(wordStub)
 {
-	var list = Array();
+	this.currentPosition = 0;
 	
-	for (var nameIndex = 0; nameIndex < this.conceptNameMapper.mapNameToConcept.keys.length; nameIndex++)
+	var startPosition = this.getFirstWordPositionStartWith(wordStub, this.conceptNameMapper.mapNameToConcept.keys, 0, this.conceptNameMapper.mapNameToConcept.keys.length - 1);
+
+	if (startPosition == -1)
+		return null;
+	
+	var selection = Array();
+	
+	for (var index = startPosition; index < this.conceptNameMapper.mapNameToConcept.keys.length; index++)
 	{
-		var name = this.conceptNameMapper.mapNameToConcept.keys[nameIndex];
+		var name = this.conceptNameMapper.mapNameToConcept.keys[index];
 		if (name.startsWith(wordStub))
-			list.push(name);
+		{
+			selection.push(name);
+		}
+		else
+		{
+			break;
+		}
+	}
+	
+	return selection;
+}
+
+//(int) Position of first word starting with word stub
+// (works with binary search)
+// -1 if nothing found
+AutoComplete.prototype.getFirstWordPositionStartWith = function AutoComplete_getFirstWordPositionStartWith(wordStub, wordList, startPosition, endPosition)
+{
+	if (startPosition == endPosition)
+	{
+		if (wordList[startPosition].startsWith(wordStub))
+			return startPosition;
+		else
+			return -1;
+	}
+	else if (startPosition + 1 == endPosition)
+	{
+		if (wordList[startPosition].startsWith(wordStub))
+			return startPosition;
+		else if (wordList[endPosition].startsWith(wordStub))
+			return endPosition;
+		else
+			return -1;
 	}
 
-	list.sort();
+	var pivot = Math.round((startPosition + endPosition) / 2);
 	
-	return list;
+	if (wordStub <= wordList[pivot].substr(0,wordStub.length))
+		return this.getFirstWordPositionStartWith(wordStub, wordList, startPosition, pivot);
+	else
+		return this.getFirstWordPositionStartWith(wordStub, wordList, pivot + 1, endPosition);
 }
