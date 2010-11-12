9c4b825720ce0efbc5a095038bd3c50ba6a4ae04
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-12 19:51:58 +0000

    AutoComplete: work in progress

--- trunk/JavaScript/javascript/AutoComplete.js
+++ trunk/JavaScript/javascript/AutoComplete.js
@@ -4,3 +4,37 @@ function AutoComplete(conceptNameMapper)
 	//Parts
 	this.conceptNameMapper = conceptNameMapper;
 }
+
+//(String) get stub of last word
+//or NULL if can't get one for now
+AutoComplete.prototype.getLastWordStub = function AutoComplete_getLastWordStub(statementString)
+{
+	statementString = ' ' + statementString;
+
+	var lastSpacePosition = statementString.lastIndexOf(' ');
+	if (lastSpacePosition != -1)
+	{
+		return statementString.substr(lastSpacePosition).trim();
+	}
+	else
+	{
+		return null;
+	}
+}
+
+//(String[]) list of words starting with wordStub
+AutoComplete.prototype.getWordList = function AutoComplete_getWordList(wordStub)
+{
+	var list = Array();
+	
+	for (var nameIndex = 0; nameIndex < this.conceptNameMapper.mapNameToConcept.keys.length; nameIndex++)
+	{
+		var name = this.conceptNameMapper.mapNameToConcept.keys[nameIndex];
+		if (name.startsWith(wordStub))
+			list.push(name);
+	}
+
+	list.sort();
+	
+	return list;
+}
