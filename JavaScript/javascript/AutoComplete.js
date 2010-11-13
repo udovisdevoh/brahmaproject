5dcc8ef7ca621ea155299cd6e0db0f8cbaa031dd
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-13 03:52:22 +0000

    AutoComplete now works fine

--- JavaScript/javascript/AutoComplete.js
+++ JavaScript/javascript/AutoComplete.js
@@ -1,11 +1,15 @@
 //Completes input
-function AutoComplete(conceptNameMapper)
+function AutoComplete(flattenizer, conceptNameMapper, proofCache)
 {
 	//Parts and fields
+	this.flattenizer = flattenizer;
 	this.conceptNameMapper = conceptNameMapper;
+	this.proofCache = proofCache;
 	this.currentPosition = 0;
 	this.isVisible = false;
 	this.selection = Array(); //List of words
+	this.selectionWithReferenceConcept = Array(); //List of words with reference concepts
+	this.paranthesesConceptCache = new Hash(); //list of reference concept, for instance: race (contest) [key: race, value: contest]
 }
 
 //(String) get stub of last word
@@ -35,6 +39,7 @@ AutoComplete.prototype.getWordList = function AutoComplete_getWordList(wordStub)
 	
 	this.selection = Array();
 	
+	
 	for (var index = startPosition; index < this.conceptNameMapper.allNames.length; index++)
 	{
 		var name = this.conceptNameMapper.allNames[index];
@@ -47,8 +52,19 @@ AutoComplete.prototype.getWordList = function AutoComplete_getWordList(wordStub)
 			break;
 		}
 	}
+
+	//We add reference concept, for instance: race (contest)
+	this.selectionWithReferenceConcept = Array();	
+	for (var index = 0; index < this.selection.length; index++)
+	{
+		var word = this.selection[index];
+		if (word.indexOf('(') == -1 || word.indexOf(')') == -1)
+			this.selectionWithReferenceConcept.push(this.getWordWithReference(word));
+		else
+			this.selectionWithReferenceConcept.push(word);
+	}
 	
-	return this.selection;
+	return this.selectionWithReferenceConcept;
 }
 
 //(int) Position of first word starting with word stub
@@ -123,4 +139,106 @@ AutoComplete.prototype.replaceLastWordWithSelectedWord = function AutoComplete_r
 	}
 	
 	return textString;
+}
+
+//(String)
+//We add reference concept, for instance: race (contest)
+AutoComplete.prototype.getWordWithReference = function AutoComplete_getWordWithReference(conceptName)
+{
+	if (this.paranthesesConceptCache.hasItem(conceptName))
+	{
+		return conceptName + ' (' + this.paranthesesConceptCache.getItem(conceptName) + ')';
+	}
+	else
+	{
+		if (this.conceptNameMapper.mapNameToConcept.keys.indexOf(conceptName) != -1)
+		{
+			var mostReleventConcept = this.getMostReleventConcept(this.conceptNameMapper.getConcept(conceptName));
+			if (mostReleventConcept != null)
+			{	
+				var referenceConceptName = mostReleventConcept.toString();
+				if (referenceConceptName.indexOf('_(') != -1 && referenceConceptName.indexOf(')') != -1)
+				{
+					referenceConceptName = referenceConceptName.substr(0, referenceConceptName.lastIndexOf('_('));
+				}
+				this.paranthesesConceptCache.setItem(conceptName, referenceConceptName);
+				return conceptName + ' (' + referenceConceptName + ')';
+			}
+		}
+	}
+	
+	return conceptName;
+}
+
+//(Concept)
+//Find the concept for which most implicit connections are derived
+AutoComplete.prototype.getMostReleventConcept = function AutoComplete_getMostReleventConcept(subject)
+{
+	var conceptConnectionCounter = new Hash();
+	
+	for (var verbIndex = 0; verbIndex < this.flattenizer.instinct.verbList.length; verbIndex++)
+	{
+		var verb = this.flattenizer.instinct.verbList[verbIndex];
+		
+		var implicitBranch = subject.getImplicitBranch(verb);
+		if (!implicitBranch.isFlat)
+			if (!implicitBranch.isLocked)
+				this.flattenizer.flattenBranch(implicitBranch, subject, verb);
+				
+		var tautologicBranch = subject.getTautologicBranch(verb);
+		
+		for (var complementIndex = 0; complementIndex < tautologicBranch.complementList.length; complementIndex++)
+		{
+			var complement = tautologicBranch.complementList[complementIndex];
+			if (!conceptConnectionCounter.hasItem(complement))
+			{
+				conceptConnectionCounter.setItem(complement,0);
+			}
+		}
+	}
+	
+	for (var verbIndex = 0; verbIndex < this.flattenizer.instinct.verbList.length; verbIndex++)
+	{
+		var verb = this.flattenizer.instinct.verbList[verbIndex];	
+		var implicitBranch = subject.getImplicitBranch(verb);
+				
+		for (var complementIndex = 0; complementIndex < implicitBranch.complementList.length; complementIndex++)
+		{
+			var complement = implicitBranch.complementList[complementIndex];
+			var proof = this.proofCache.getProof(subject, verb, complement, true);
+			
+			if (proof != null)
+			{
+				for (var argumentIndex = 0; argumentIndex < proof.length; argumentIndex++)
+				{
+					var argument = proof[argumentIndex];
+					if (conceptConnectionCounter.hasItem(argument.subject))
+					{
+						conceptConnectionCounter.setItem(argument.subject, conceptConnectionCounter.getItem(argument.subject) + 1);
+					}
+					else if (conceptConnectionCounter.hasItem(argument.complement))
+					{
+						conceptConnectionCounter.setItem(argument.complement, conceptConnectionCounter.getItem(argument.complement) + 1);
+					}
+				}
+			}
+		}
+	}
+	
+	var mostReleventConcept = null;
+	var biggestConnectionCount = -1;
+	
+	for (var index = 0; index < conceptConnectionCounter.keys.length; index++)
+	{
+		var complement = conceptConnectionCounter.keys[index];
+		var currentConnectionCount = conceptConnectionCounter.getItem(complement);
+		
+		if (currentConnectionCount > biggestConnectionCount)
+		{
+			biggestConnectionCount = currentConnectionCount;
+			mostReleventConcept = complement;
+		}
+	}
+
+	return mostReleventConcept;
 }
