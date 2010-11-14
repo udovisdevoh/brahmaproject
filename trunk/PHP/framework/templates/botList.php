a531af920bbddd57c66907d8c67f76b5b3307a83
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-14 20:00:10 +0000

    Site: work in progress

--- trunk/PHP/framework/templates/botList.php
+++ trunk/PHP/framework/templates/botList.php
@@ -1,8 +1,8 @@
 <ul class="BotList">
 	<?php foreach ($aiUnitList as $aiUnit): ?>
 	<li>
-		<a href="./bot.php?ai=<?php echo $aiUnit['key_name'] ?>"><?php echo $aiUnit['name'] ?></a>
-		rating: <?php echo $aiUnit['rating'] ?>
+		<p><a href="./bot.php?ai=<?php echo $aiUnit['key_name'] ?>"><?php echo $aiUnit['name'] ?></a></p>
+		<p>rating: <?php echo $aiUnit['rating'] ?></p>
 	</li>
 	<?php endforeach ?>
 </ul>
