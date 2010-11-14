5b77c72aa36633180d6ee37ad515e1b424fd3dba
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-14 16:08:44 +0000

    PHP: work in progress

--- trunk/PHP/framework/templates/menu.php
+++ trunk/PHP/framework/templates/menu.php
@@ -1,11 +1,23 @@
 <ul class="Menu">
 	<li>
+		<a href="chatwithbrahma.php">Chat with Brahma</a>
+	</li>
+	<li>
+		<a href="viewavatars.php">Chat with a bot</a>
+	</li>
+	<li>
+		<a href="viewavatars.php">Create your bot</a>
+	</li>
+	<?php if (isset($isLogged) && $isLogged):?>
+	<li>
+		<a href="signout.php">Sign out</a>
+	</li>
+	<?php else: ?>
+	<li>
 		<a href="createaccount.php">Create account</a>
 	</li>
 	<li>
 		<a href="signin.php">Sign in</a>
 	</li>
-	<li>
-		<a href="signout.php">Sign out</a>
-	</li>
+	<?php endif ?>
 </ul>
