b00db12cd84fd2ef64f508fb50b800edf87ce460
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-14 18:00:00 +0000

    Site: work in progress

--- trunk/PHP/framework/templates/header.php
+++ trunk/PHP/framework/templates/header.php
@@ -1,1 +1,11 @@
-<img src="./images/brahmaproject.png" alt="Brahma Project" />
+<img class="SiteLogo" src="./images/brahmaproject.png" alt="Brahma Project" />
+
+<div class="Header">
+	<h1>Collective Artificial Intelligence</h1>
+	<p>
+		Create a chat bot at your image and contribute to the Brahma Project.
+	</p>
+	<?php require_once("./framework/templates/menu.php"); ?>
+</div>
+
+<div style="clear:both"></div>
