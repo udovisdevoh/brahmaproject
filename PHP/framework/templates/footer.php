01de911447c5c828ef081ddedf59be6476ca3339
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-15 02:55:28 +0000

    Improved style

--- PHP/framework/templates/footer.php
+++ PHP/framework/templates/footer.php
@@ -1,3 +1,5 @@
+<div style="clear:both"></div>
+
 <p class="Copyright">
 	<a href="http://www.neotenic.net">Neotenic.Net</a> - <a href="http://www.anticulture.net">AntiCulture.Net</a> &copy; 2007-<?php echo date('Y') ?> all rights reserved
 </p>
