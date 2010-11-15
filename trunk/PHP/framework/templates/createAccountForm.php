f7ef6542e3b2bb577af65bdfbd92444ad8ff437d
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-15 23:51:23 +0000

    Site: work in progress

--- trunk/PHP/framework/templates/createAccountForm.php
+++ trunk/PHP/framework/templates/createAccountForm.php
@@ -3,27 +3,27 @@
 	<fieldset>
 		<div>
 			<label for="name">Display name</label>
-			<input type="text" id="name" name="name" value="" />
+			<input type="text" id="name" name="name" value="<?php echo isset($_POST['name']) ? strip_tags($_POST['name']) : ''; ?>" />
 		</div>
 		<div>
 			<label for="password">Password (minimum 12 chars, must contain lower case, capital and number</label>
-			<input type="password" id="password1" name="password1" value="" />
+			<input type="password" id="password1" name="password1" value="<?php echo isset($_POST['password1']) ? strip_tags($_POST['password1']) : ''; ?>" />
 		</div>
 		<div>
 			<label for="password2">Password confirmation</label>
-			<input type="password" id="password2" name="password2" value="" />
+			<input type="password" id="password2" name="password2" value="<?php echo isset($_POST['password2']) ? strip_tags($_POST['password2']) : ''; ?>" />
 		</div>
 		<div>
 			<label for="email">Email</label>
-			<input type="text" id="email" name="email" value="" />
+			<input type="text" id="email" name="email" value="<?php echo isset($_POST['email']) ? strip_tags($_POST['email']) : ''; ?>" />
 		</div>
 		<div>
 			<label for="first_name">First name</label>
-			<input type="text" id="first_name" name="first_name" value="" />
+			<input type="text" id="first_name" name="first_name" value="<?php echo isset($_POST['first_name']) ? strip_tags($_POST['first_name']) : ''; ?>" />
 		</div>
 		<div>
 			<label for="last_name">Last name</label>
-			<input type="text" id="last_name" name="last_name" value="" />
+			<input type="text" id="last_name" name="last_name" value="<?php echo isset($_POST['last_name']) ? strip_tags($_POST['last_name']) : ''; ?>" />
 		</div>
 	</fieldset>
 	<div>
