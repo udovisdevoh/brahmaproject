3026728a659bad1f97102e02dba5c65465757832
glacasse@gmail.com <glacasse@gmail.com@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-16 15:52:14 +0000

    Improved faq page

--- trunk/PHP/faq.php
+++ trunk/PHP/faq.php
@@ -4,7 +4,7 @@
 	<head>
 		<?php require_once("./framework/templates/headTags.php"); ?>
 
-		<title>Brahma Project: My account</title>
+		<title>Brahma Project: FAQ</title>
 	</head>
 	<body>
 		<?php require_once("./framework/templates/header.php"); ?>
@@ -14,21 +14,26 @@
 			<h2>What's the concept here?</h2>
 			
 			<p>
-				By creating an account, you can train artificial intelligence chat bots at your image. We call them "left brains".
-				People can chat with them and rate them. Doing so contributes to the building of a society of AI bots.
-				Besides of chatting with individual bots, you can also chat with Brahma, the synthesis of all these chat bots.
+				By <a href="./createaccount.php">creating an account</a>, you can train artificial intelligence chat bots at your image. We call them "left brains".
+				People can <a href="./">chat with them</a> and rate them. Training and rating them contributes to the building of a society of AI bots
+				for which <a href="./chat.php">Brahma is the centralized chat bot</a>, a synthesis of all the left brains.
 			<p>
 			
 			<h2>How do left brains work?</h2>
 			
 			<p>
-				
+				We call them left brains because they learn in a very formal way. You can teach them to do <a href="./tutorial.php">logical derivation, dissertation skeletons, statistical inference and inductive reasoning</a>.
+				While they have powerful logic skills, they lack the ability to understand analogies, to switch to different thinking contexts and to see things in a more global perspective.
+				To say they see reality in a way that is similar to people having <a href="http://en.wikipedia.org/wiki/Asperger_syndrome">asperger syndrome (wikipedia)</a>
+				could be an interesting analogy to make.
 			<p>
 			
 			<h2>How does Brahma work?</h2>
 			
 			<p>
-				
+				In counterpart, <a href="./chat.php">Brahma</a> is able to switch from one context to one other, and can see reality in a much more <a href="http://en.wikipedia.org/wiki/Holism">holistic (wikipedia)</a> right brain way.
+				Brahma uses all the left brains created by people here in order to sustain an even more human-like conversation. Left brains are basically Brahma's neurons. Much more like a real human being,
+				Brahma can cope with contradiction and ambiguity.
 			<p>	
 		</div>
 		
