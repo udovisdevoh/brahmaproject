0836960881f3b3cae47c87744c5d35d2d858bf88
glacasse@gmail.com <glacasse@gmail.com@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-18 19:35:37 +0000

    Added quick documentation page

---
+++ trunk/PHP/documentation.php
@@ -0,0 +1,20 @@
+<?php require_once("./framework/controllers/documentation.php"); ?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<?php require_once("./framework/templates/headTags.php"); ?>
+
+		<title>Brahma Project: Quick Documentation</title>
+	</head>
+	<body>
+		<?php require_once("./framework/templates/header.php"); ?>
+		<div style="clear:both"></div>
+		
+		<div id="documentation" class="Container">
+			<h2>Quick documentation</h2>	
+		</div>
+		
+		<?php require_once("./framework/templates/footer.php"); ?>
+		
+	</body>
+</html>
--- trunk/PHP/framework/templates/menu.php
+++ trunk/PHP/framework/templates/menu.php
@@ -5,14 +5,14 @@
 	<li>
 		<a href="./"><img src="./images/createbot.png" alt="Bots" />Chat with left brains</a>
 	</li>
-	<!--<li>
-		<a href="./"><img src="./images/findbot.png" alt="Find bot" />Find bot</a>
-	</li>-->
 	<li>
-		<a href="./faq.php"><img src="./images/aboutus.png" alt="FAQ" />WTF???</a>
+		<a href="./faq.php"><img src="./images/faq.png" alt="FAQ" />FAQ</a>
 	</li>
 	<li>
-		<a href="./videotutorial.php"><img src="./images/videotutorial.png" alt="Video tutorial" />Video tutorial</a>
+		<a href="./documentation.php"><img src="./images/documentation.png" alt="Quick documentation" />Quick documentation</a>
+	</li>
+	<li>
+		<a href="./videotutorial.php"><img src="./images/videotutorial.png" alt="Video tutorials" />Video tutorials</a>
 	</li>
 </ul>
 <div style="clear:both"></div>
