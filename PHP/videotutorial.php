ac0def2fd09ad6fb67e4d75f41960070b1bc0f6d
glacasse@gmail.com <glacasse@gmail.com@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-18 19:35:37 +0000

    Added quick documentation page

--- PHP/videotutorial.php
+++ PHP/videotutorial.php
@@ -9,14 +9,27 @@
 	<body>
 		<?php require_once("./framework/templates/header.php"); ?>
 		<div style="clear:both"></div>
-		
-		<div id="videoTutorial" class="Container">
-			<object width="425" height="344">
-				<param name="movie" value="http://www.youtube.com/v/GwQMnpUsj8I&hl=en&fs=1"></param>
-				<param name="allowFullScreen" value="true"></param>
-				<param name="allowscriptaccess" value="always"></param>
-				<embed src=http://www.youtube.com/v/GwQMnpUsj8I&hl=en&fs=1 type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed>
-			</object> 
+
+		<div id="videotutorial" class="Container">		
+			<h2>Video Tutorials</h2>
+			
+			<ul>
+				<li>
+					Train your first left brain bot
+				</li>
+				<li>
+					Advanced use of the left brain interface
+				</li>
+			</ul>
+			
+			<div id="videoTutorial" class="Container">
+				<object width="425" height="344">
+					<param name="movie" value="http://www.youtube.com/v/GwQMnpUsj8I&hl=en&fs=1"></param>
+					<param name="allowFullScreen" value="true"></param>
+					<param name="allowscriptaccess" value="always"></param>
+					<embed src=http://www.youtube.com/v/GwQMnpUsj8I&hl=en&fs=1 type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="425" height="344"></embed>
+				</object> 
+			</div>
 		</div>
 		
 		<?php require_once("./framework/templates/footer.php"); ?>
