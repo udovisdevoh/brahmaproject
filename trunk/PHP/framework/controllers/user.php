01f78bdf091ec314add78d38a68376e88f33c6bd
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-21 14:47:56 +0000

    User profile on separate controller and url

---
+++ trunk/PHP/framework/controllers/user.php
@@ -0,0 +1,36 @@
+<?php
+require_once("./framework/settings.php");
+
+if (isset($_GET['user'])) //Single User
+{
+	$offset = isset($_GET['offset']) ? (int)$_GET['offset'] : 0;
+	$offset = ((int)($offset / AI_SHORT_PROFILE_COUNT_PER_PAGE)) * AI_SHORT_PROFILE_COUNT_PER_PAGE;
+	
+	$userKey = (int)$_GET['user'];
+	$renderedView = Cache::get('user_profile_'.$userKey.'_'.$offset, DEFAULT_CACHE_TIMEOUT);
+	if ($renderedView == null)
+	{
+		$viewedUserProfile = UserProfile::getUser('`id` = '.$userKey.' and `is_active` = 1');	
+		if ($viewedUserProfile == null)
+			die();	
+		$aiUnitList = AiUnit::getAiUnitListForUser($userKey, $offset, AI_SHORT_PROFILE_COUNT_PER_PAGE);
+		$bestUpRating = AiUnit::getBestUpRating();
+		$worstDownRating = AiUnit::getWorstDownRating();
+		$aiUnitCount = AiUnit::countForUser($userKey);
+
+		
+		$totalUpRating = UserProfile::getTotalUpRating($userKey);
+		$totalDownRating = UserProfile::getTotalDownRating($userKey);
+		$rank = UserProfile::getRank($userKey);
+		
+		$renderedView = UserProfileViewer::view($viewedUserProfile, $aiUnitList, $offset, AI_SHORT_PROFILE_COUNT_PER_PAGE, $aiUnitCount, $bestUpRating, $worstDownRating, $totalUpRating, $totalDownRating, $rank);
+		
+		if ($aiUnitList != null)
+			Cache::set('user_profile_'.$userKey.'_'.$offset, $renderedView);
+	}
+}
+else
+{
+	//Implement list of users
+}
+?>
---
+++ trunk/PHP/user.php
@@ -0,0 +1,21 @@
+<?php require_once("./framework/controllers/user.php"); ?>
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.2//EN" "http://www.openmobilealliance.org/tech/DTD/xhtml-mobile12.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+	<head>
+		<?php require_once("./framework/templates/headTags.php"); ?>
+
+		<title>Collective Artificial Intelligence: Brahma Project</title>
+	</head>
+	<body>
+		<?php require_once("./framework/templates/header.php"); ?>
+		
+		<div style="clear:both"></div>
+		
+		<div class="Container">
+			<?php echo $renderedView ?>
+		</div>
+		
+		<?php require_once("./framework/templates/footer.php"); ?>
+		
+	</body>
+</html>
