22c5ab0a9d98c7ba9f442317f490c3fa0e28a8a1
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-22 14:33:53 +0000

    Moved some JS function to LeftBrain's Global.js

---
+++ trunk/Sites/js/leftBrain/Globals.js
@@ -0,0 +1,128 @@
+//Handles key up events
+function keyUpHandling(e)
+{
+	var unicode = getKeyCode(e);
+	var key = String.fromCharCode(unicode);
+
+	var rememberedStatement;
+	
+	if (unicode != 38 && unicode != 40 && unicode != 27 && unicode != 13)
+	{
+		var lastWordStub = talkingRouter.autoComplete.getLastWordStub(inputField.value);
+		if (lastWordStub != null/* && lastWordStub.length > 1*/)
+		{
+			var wordList = talkingRouter.autoComplete.getWordList(lastWordStub);
+			if (wordList == null)
+			{
+				autoCompleteDom.style.visibility = 'hidden';
+				talkingRouter.autoComplete.isVisible = false;
+			}
+			else
+			{
+				talkingRouter.autoComplete.isVisible = true;
+				autoCompleteDom.style.visibility = 'visible';
+				autoCompleteDom.innerHTML = '';
+				for (var wordIndex = 0; wordIndex < wordList.length; wordIndex++)
+					talkingRouter.autoComplete.addViewElement(autoCompleteDom, wordIndex, wordList[wordIndex], wordIndex == talkingRouter.autoComplete.currentPosition);
+			}
+		}
+		else
+		{
+			talkingRouter.autoComplete.isVisible = false;
+			autoCompleteDom.style.visibility = 'hidden';
+		}
+	}
+}
+
+//Handles key down events
+function keyDownHandling(e)
+{
+	var unicode = getKeyCode(e);
+	var key = String.fromCharCode(unicode);
+
+	var rememberedStatement;
+	
+	if (unicode == 38) //up
+	{			
+		if (talkingRouter.autoComplete.isVisible)
+		{
+			talkingRouter.autoComplete.moveUp(autoCompleteDom);
+		}
+		else
+		{
+			rememberedStatement = talkingRouter.history.getStatement();
+			if (rememberedStatement != null)
+			{
+				inputField.value = rememberedStatement;
+				talkingRouter.history.moveUp();
+			}
+		}
+		return false;
+	}
+	else if (unicode == 40) //down
+	{
+		if (talkingRouter.autoComplete.isVisible)
+		{
+			talkingRouter.autoComplete.moveDown(autoCompleteDom);
+		}
+		else
+		{
+			rememberedStatement = talkingRouter.history.getStatement();
+			if (rememberedStatement != null)
+			{
+				inputField.value = rememberedStatement;
+				talkingRouter.history.moveDown();
+			}
+		}
+		return false;
+	}
+	else if (unicode == 27) //esc
+	{
+		if (talkingRouter.autoComplete.isVisible)
+		{
+			inputField.focus();
+			talkingRouter.autoComplete.isVisible = false;
+			autoCompleteDom.style.visibility = 'hidden';
+			return false;
+		}
+	}
+	else if (unicode == 13 || unicode == 32) //enter or space
+	{
+		if (talkingRouter.autoComplete.isVisible)
+		{
+			inputField.focus();
+			talkingRouter.autoComplete.isVisible = false;
+			autoCompleteDom.style.visibility = 'hidden';
+			inputField.value = talkingRouter.autoComplete.replaceLastWordWithSelectedWord(inputField.value);
+			
+			if (unicode == 32)//space
+				inputField.value += ' ';
+
+			if (unicode == 13 && talkingRouter.autoComplete.isCompleteStatement(inputField.value))//enter
+				return true;//don't cancel enter button, send message to ai
+				
+			return false; //cancel enter button
+		}
+	}
+}
+
+function autoCompleteClick(elementId)
+{
+	if (talkingRouter.autoComplete.isVisible)
+	{
+		talkingRouter.autoComplete.moveTo(autoCompleteDom, elementId);
+	}
+}
+
+function autoCompleteDoubleClick()
+{
+	if (talkingRouter.autoComplete.isVisible)
+	{
+		inputField.focus();
+		talkingRouter.autoComplete.isVisible = false;
+		autoCompleteDom.style.visibility = 'hidden';
+		inputField.value = talkingRouter.autoComplete.replaceLastWordWithSelectedWord(inputField.value);
+		
+		return false;
+	}
+}
--- trunk/Sites/leftbrainchat.php
+++ trunk/Sites/leftbrainchat.php
@@ -30,17 +30,6 @@
 					conversationDom.style.height = Math.max(0, self.innerHeight - 235) + 'px';
 			}
 			
-			var humanName = "<?php echo strtolower($userProfile['name'])?>";
-			var aiName = "<?php echo strtolower($aiUnit['name'])?>";
-			var talkingRouter = new TalkingRouter(humanName, aiName);
-			var inputField = document.getElementById('humanStatementField');
-			var autoCompleteDom = document.getElementById('autoComplete');
-			var conversationDom = document.getElementById('conversation');
-			inputField.focus();
-			inputField.onkeyup = keyUpHandling;
-			document.onkeydown = keyDownHandling;		
-			resizeTextFields();
-			
 			//Send message to Ai
 			function sendMessage(talkingRouter)
 			{
@@ -57,135 +46,16 @@
 				return false;
 			}
 			
-			//Handles key up events
-			function keyUpHandling(e)
-			{
-				var unicode = getKeyCode(e);
-				var key = String.fromCharCode(unicode);
-
-				var rememberedStatement;
-				
-				if (unicode != 38 && unicode != 40 && unicode != 27 && unicode != 13)
-				{
-					var lastWordStub = talkingRouter.autoComplete.getLastWordStub(inputField.value);
-					if (lastWordStub != null/* && lastWordStub.length > 1*/)
-					{
-						var wordList = talkingRouter.autoComplete.getWordList(lastWordStub);
-						if (wordList == null)
-						{
-							autoCompleteDom.style.visibility = 'hidden';
-							talkingRouter.autoComplete.isVisible = false;
-						}
-						else
-						{
-							talkingRouter.autoComplete.isVisible = true;
-							autoCompleteDom.style.visibility = 'visible';
-							autoCompleteDom.innerHTML = '';
-							for (var wordIndex = 0; wordIndex < wordList.length; wordIndex++)
-								talkingRouter.autoComplete.addViewElement(autoCompleteDom, wordIndex, wordList[wordIndex], wordIndex == talkingRouter.autoComplete.currentPosition);
-						}
-					}
-					else
-					{
-						talkingRouter.autoComplete.isVisible = false;
-						autoCompleteDom.style.visibility = 'hidden';
-					}
-				}
-			}
-			
-			//Handles key down events
-			function keyDownHandling(e)
-			{
-				var unicode = getKeyCode(e);
-				var key = String.fromCharCode(unicode);
-
-				var rememberedStatement;
-				
-				if (unicode == 38) //up
-				{			
-					if (talkingRouter.autoComplete.isVisible)
-					{
-						talkingRouter.autoComplete.moveUp(autoCompleteDom);
-					}
-					else
-					{
-						rememberedStatement = talkingRouter.history.getStatement();
-						if (rememberedStatement != null)
-						{
-							inputField.value = rememberedStatement;
-							talkingRouter.history.moveUp();
-						}
-					}
-					return false;
-				}
-				else if (unicode == 40) //down
-				{
-					if (talkingRouter.autoComplete.isVisible)
-					{
-						talkingRouter.autoComplete.moveDown(autoCompleteDom);
-					}
-					else
-					{
-						rememberedStatement = talkingRouter.history.getStatement();
-						if (rememberedStatement != null)
-						{
-							inputField.value = rememberedStatement;
-							talkingRouter.history.moveDown();
-						}
-					}
-					return false;
-				}
-				else if (unicode == 27) //esc
-				{
-					if (talkingRouter.autoComplete.isVisible)
-					{
-						inputField.focus();
-						talkingRouter.autoComplete.isVisible = false;
-						autoCompleteDom.style.visibility = 'hidden';
-						return false;
-					}
-				}
-				else if (unicode == 13 || unicode == 32) //enter or space
-				{
-					if (talkingRouter.autoComplete.isVisible)
-					{
-						inputField.focus();
-						talkingRouter.autoComplete.isVisible = false;
-						autoCompleteDom.style.visibility = 'hidden';
-						inputField.value = talkingRouter.autoComplete.replaceLastWordWithSelectedWord(inputField.value);
-						
-						if (unicode == 32)//space
-							inputField.value += ' ';
-
-						if (unicode == 13 && talkingRouter.autoComplete.isCompleteStatement(inputField.value))//enter
-							return true;//don't cancel enter button, send message to ai
-							
-						return false; //cancel enter button
-					}
-				}
-			}
-			
-			function autoCompleteClick(elementId)
-			{
-				if (talkingRouter.autoComplete.isVisible)
-				{
-					talkingRouter.autoComplete.moveTo(autoCompleteDom, elementId);
-				}
-			}
-			
-			function autoCompleteDoubleClick()
-			{
-				if (talkingRouter.autoComplete.isVisible)
-				{
-					inputField.focus();
-					talkingRouter.autoComplete.isVisible = false;
-					autoCompleteDom.style.visibility = 'hidden';
-					inputField.value = talkingRouter.autoComplete.replaceLastWordWithSelectedWord(inputField.value);
-					
-					return false;
-				}
-			}
-			
+			var humanName = "<?php echo strtolower($userProfile['name'])?>";
+			var aiName = "<?php echo strtolower($aiUnit['name'])?>";
+			var talkingRouter = new TalkingRouter(humanName, aiName);
+			var inputField = document.getElementById('humanStatementField');
+			var autoCompleteDom = document.getElementById('autoComplete');
+			var conversationDom = document.getElementById('conversation');
+			inputField.focus();
+			inputField.onkeyup = keyUpHandling;
+			document.onkeydown = keyDownHandling;		
+			resizeTextFields();
 			-->
 		</script>
 		
