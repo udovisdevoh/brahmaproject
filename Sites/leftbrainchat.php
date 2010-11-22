f681d61555a557b8728cd1277c32ca7271fcda4e
glacasse <glacasse@ba5b47a9-56c4-8051-8d7a-872ac3991d47>
2010-11-22 14:13:48 +0000

    Moved leftbrain javascript

--- Sites/leftbrainchat.php
+++ Sites/leftbrainchat.php
@@ -3,44 +3,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 	<head>
 		<?php require_once("./framework/templates/headTags.php"); ?>
-
+		<?php require_once("./framework/templates/leftBrainHeadTags.php"); ?>
 		<title>Collective Artificial Intelligence: Brahma Project</title>
-		
-		<link href="./css/leftbrain.css" rel="stylesheet" type="text/css" />
-		
-		<script type="text/javascript" src="./js/Main.js"></script>
-		<script type="text/javascript" src="./js/Concept.js"></script>
-		<script type="text/javascript" src="./js/VerbBranch.js"></script>
-		<script type="text/javascript" src="./js/ConceptNameMapper.js"></script>
-		<script type="text/javascript" src="./js/TautologyManager.js"></script>
-		<script type="text/javascript" src="./js/Instinct.js"></script>
-		<script type="text/javascript" src="./js/Statement.js"></script>
-		<script type="text/javascript" src="./js/ComplementaryOperatorManager.js"></script>
-		<script type="text/javascript" src="./js/MutuallyExclusiveOperatorManager.js"></script>
-		<script type="text/javascript" src="./js/EvaluationCache.js"></script>
-		<script type="text/javascript" src="./js/ProofCache.js"></script>
-		<script type="text/javascript" src="./js/Flattenizer.js"></script>
-		<script type="text/javascript" src="./js/Optimizer.js"></script>
-		<script type="text/javascript" src="./js/Invalidator.js"></script>
-		<script type="text/javascript" src="./js/ProofManager.js"></script>
-		<script type="text/javascript" src="./js/ThinkerGeneralizationToParent.js"></script>
-		<script type="text/javascript" src="./js/ThinkerGeneralizationToBrother.js"></script>
-		<script type="text/javascript" src="./js/ThinkerFindParentOfEnemyBrother.js"></script>
-		<script type="text/javascript" src="./js/ThinkerFindEnemyBrother.js"></script>
-		<script type="text/javascript" src="./js/Thinker.js"></script>
-		<script type="text/javascript" src="./js/Theory.js"></script>
-		<script type="text/javascript" src="./js/WhatisViewer.js"></script>
-		<script type="text/javascript" src="./js/DefineViewer.js"></script>
-		<script type="text/javascript" src="./js/TeachViewer.js"></script>
-		<script type="text/javascript" src="./js/ObjectionFinder.js"></script>
-		<script type="text/javascript" src="./js/FirstSecondPersonManager.js"></script>
-		<script type="text/javascript" src="./js/HumanStatementSplitter.js"></script>
-		<script type="text/javascript" src="./js/HumanStatementColorizer.js"></script>
-		<script type="text/javascript" src="./js/AskViewer.js"></script>
-		<script type="text/javascript" src="./js/History.js"></script>
-		<script type="text/javascript" src="./js/AutoComplete.js"></script>
-		<script type="text/javascript" src="./js/ConversationManager.js"></script>
-		<script type="text/javascript" src="./js/TalkingRouter.js"></script>
 	</head>
 	<body onresize="resizeTextFields()">
 		<?php /*require_once("./framework/templates/header.php");*/ ?>
